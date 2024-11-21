a22447789c3c96c7c87fdf56009901637c31aceb
WruddhiR <104713330+WruddhiR@users.noreply.github.com>
2024-11-21 20:55:38 +0530

    Update emotion_detector.py

--- emotion_detector.py
+++ emotion_detector.py
@@ -1,1 +1,17 @@
+import unittest
+from emotion_detection.emotion_detector import emotion_predictor
+
+class TestEmotionPredictor(unittest.TestCase):
+    def test_anger(self):
+        text = "I am so angry!"
+        result = emotion_predictor(text)
+        self.assertGreater(result['anger'], 0)
+
+    def test_joy(self):
+        text = "I am so happy!"
+        result = emotion_predictor(text)
+        self.assertGreater(result['joy'], 0)
+
+if __name__ == '__main__':
+    unittest.main()
 
